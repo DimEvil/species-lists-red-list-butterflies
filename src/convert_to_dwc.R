dfd563c0568763044a1ebb514861013efa465049
DimEvil <dimibrosens@gmail.com>
2018-03-14 14:52:55 +0100

    Create subdataset DwcTaxon & DwCTaxonBE

--- src/convert_to_dwc.R
+++ src/convert_to_dwc.R
@@ -2,5 +2,8 @@
 
 library(tidyverse)
 
-
 dataset <- read.csv("~/GitHub/species-lists-red-list-butterflies/species-lists-red-list-butterflies/Data/dataset.txt", sep=";")
+
+DwcTaxon <- dataset[,c(1,2)]
+DwcTaxonBE <- select(filter(dataset, CountryCode == 'BE'),c(1,2,3,4))
+
