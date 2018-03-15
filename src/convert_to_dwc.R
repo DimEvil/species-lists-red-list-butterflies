620de55a65f9ea1e496507e3d9613e55282f3395
DimEvil <dimibrosens@gmail.com>
2018-03-15 12:06:12 +0100

    Add  library rgbif

--- src/convert_to_dwc.R
+++ src/convert_to_dwc.R
@@ -11,13 +11,18 @@ library(knitr)     # For nicer (kable) tables
 library(readxl)    # To read excel files
 library(stringr)   # To perform string operations
 library(digest)    # To generate hashes
+library(rgbif)     # To Match GBIF
 
 rawdataset <- read.csv("~/GitHub/species-lists-red-list-butterflies/species-lists-red-list-butterflies/Data/dataset.txt", sep=";")
+
+names(rawdataset)[1] <- "scientificNameLocal"
+names(rawdataset)[2] <- "scientificName"  #is scientificName EU
 
 DwcTaxon <- rawdataset[,c(1,2)]
 DwcTaxonBE <- select(filter(rawdataset, CountryCode == 'BE'),c(1,2,3,4))
 
 vdigest <- Vectorize(digest)
+DwcTaxon %<>% mutate(license = "http://creativecommons.org/publicdomain/zero/1.0/", rightsHolder = "INBO", accessRights = "http://www.inbo.be/en/norms-for-data-use", datasetName = "Butterfly Species List Europe", occurrencestatus = "present", language = 'EN', datasetID = "DOI", kingdom = "Animalia", phylum = "Artropoda" , class = 'Insecta" ' )
 
 
 
