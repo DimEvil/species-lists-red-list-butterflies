17882086ed1d7157f108eac9dbb1298d38e3196b
DimEvil <dimibrosens@gmail.com>
2018-03-15 12:00:08 +0100

    Add libraries

--- src/convert_to_dwc.R
+++ src/convert_to_dwc.R
@@ -1,9 +1,24 @@
 
 
-library(tidyverse)
+library(tidyverse) # For data transformations
 
-dataset <- read.csv("~/GitHub/species-lists-red-list-butterflies/species-lists-red-list-butterflies/Data/dataset.txt", sep=";")
+# None core tidyverse packages:
+library(magrittr)  # For %<>% pipes
 
-DwcTaxon <- dataset[,c(1,2)]
-DwcTaxonBE <- select(filter(dataset, CountryCode == 'BE'),c(1,2,3,4))
+# Other packages
+library(janitor)   # For cleaning input data
+library(knitr)     # For nicer (kable) tables
+library(readxl)    # To read excel files
+library(stringr)   # To perform string operations
+library(digest)    # To generate hashes
+
+rawdataset <- read.csv("~/GitHub/species-lists-red-list-butterflies/species-lists-red-list-butterflies/Data/dataset.txt", sep=";")
+
+DwcTaxon <- rawdataset[,c(1,2)]
+DwcTaxonBE <- select(filter(rawdataset, CountryCode == 'BE'),c(1,2,3,4))
+
+vdigest <- Vectorize(digest)
+
+
+
 
